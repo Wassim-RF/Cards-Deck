6bfb107ab9f05d39bc33adb964e911bc7573c7a3
Wassim-RF <wassimrifi0313@gmail.com>
2025-11-08 22:41:43 +0100

    j'ai metre a jour la partie du link de collection et j'ai termine la partie du localstorage de structure du link et info du collection

--- assets/js/create_collection-card.js
+++ assets/js/create_collection-card.js
@@ -19,69 +19,101 @@ retourCreateCollectiomButton.addEventListener("click" , () => {
 
 // Collection info 
 const CollectionNameUpload = document.getElementById("Collection_Name");
-const CollectionImagesUpload = document.getElementById("Collection_images");
 const CollectionDescriptionUpload = document.getElementById("Collection_description");
 const CollectionDivContainer = document.getElementById("Collection_div--container"); // div de container collection
 const saveCollectionButton = document.getElementById("save_collection--Button"); // save collection buton
-let collectionNumber = 0 ;
+let collectionNumber = 0;
+let collectionCardNumber = 0;
 
-function uploadImage() {
-    let imgLink = URL.createObjectURL(CollectionImagesUpload.files[0]);
-    return imgLink;
-}
-
-CollectionImagesUpload.addEventListener("input" , uploadImage);
-
-
-saveCollectionButton.addEventListener("click" , (e) => {
-    e.preventDefault(); // m reloadich page
-
-    // creation dyal link dkol collection
+function creerCollectionDiv() {
     const collectionLinks = document.createElement("a");
-    collectionLinks.classList.add("w-full" , "h-[310px]" , "border" , "border-[#c0c0c0]" ,  "rounded-2xl" , "shadow-[1px_1px_4px_#c0c0c0]" , "hover:scale-[1.02]" , "cursor-pointer");
+    collectionLinks.classList.add("w-full" , "h-[120px]" , "p-1.5" , "border" , "border-[#c0c0c0]" ,  "rounded-2xl" , "shadow-[1px_1px_4px_#c0c0c0]" , "hover:scale-[1.02]" , "cursor-pointer");
     collectionLinks.setAttribute("href" , "oneCollection.html");
     CollectionDivContainer.appendChild(collectionLinks);
 
-    const collectionLinkDiv = document.createElement("div");
-    collectionLinks.appendChild(collectionLinkDiv);
+    const collectionLinkHeader = document.createElement("div");
+    collectionLinkHeader.classList.add("flex" , "justify-between" , "items-center");
+    collectionLinks.appendChild(collectionLinkHeader);
 
-    const collectionDivImage = document.createElement("div");
-    collectionDivImage.classList.add("w-full" , "flex" , "justify-center" , "items-center" , "mt-2.5");
-    collectionLinkDiv.appendChild(collectionDivImage);
+    const collectionLinkDescription = document.createElement("p");
+    collectionLinkDescription.classList.add("text-xs" , "mt-1" , "mb-1" , "w-full" , "whitespace-normal", "wrap-break-word");
+    collectionLinkDescription.innerText = CollectionDescriptionUpload.value;
+    collectionLinks.appendChild(collectionLinkDescription);
 
-    const collectionImage = document.createElement("img");
-    collectionImage.classList.add("w-[90%]" , "max-h-[150px]");
-    let imgLink = uploadImage();
-    collectionImage.setAttribute("src" , imgLink);
-    collectionDivImage.appendChild(collectionImage);
+    const collectionLinkBody = document.createElement("div");
+    collectionLinkBody.classList.add("flex" , "justify-between" , "items-center");
+    collectionLinks.appendChild(collectionLinkBody);
 
-    const collectionDivText = document.createElement("div");
-    collectionDivText.classList.add("ml-2.5" , "mt-2.5");
-    collectionLinkDiv.appendChild(collectionDivText);
+    const collectionTitle = document.createElement("h1");
+    collectionTitle.classList.add("text-2xl" , "font-medium");
+    collectionTitle.innerText = CollectionNameUpload.value;
+    collectionLinkHeader.appendChild(collectionTitle);
 
-    const CollectionName = document.createElement("h1");
-    CollectionName.classList.add("text-xl" , "font-medium");
-    CollectionName.textContent = CollectionNameUpload.value;
-    collectionDivText.appendChild(CollectionName);
+    const collectionMiniSetting = document.createElement("p");
+    collectionMiniSetting.classList.add("text-[20px]");
+    collectionMiniSetting.innerText = "...";
+    collectionLinkHeader.appendChild(collectionMiniSetting);
 
-    const collectionDescription = document.createElement("h1");
-    collectionDescription.classList.add("text-xs" , "mt-1" , "mb-1" , "w-full" , "whitespace-normal" , "wrap-break-word");
-    collectionDescription.textContent = CollectionDescriptionUpload.value;
-    collectionDivText.appendChild(collectionDescription);
+    const collectionNumberCards = document.createElement("p");
+    collectionNumberCards.classList.add("w-1/3" , "bg-[#c0c0c0]" , "text-center" , "rounded-3xl" , "mt-2");
+    collectionNumberCards.innerText = collectionCardNumber + " Cards";
+    collectionLinkBody.appendChild(collectionNumberCards);
 
-    const cardsNumber = document.createElement("h1");
-    cardsNumber.classList.add("w-1/3" , "bg-[#c0c0c0]" , "text-center" , "rounded-3xl" , "mt-2");
-    cardsNumber.textContent = collectionNumber + " cards";
-    collectionDivText.appendChild(cardsNumber);
-    collectionNumber++;
 
     CollectionNameUpload.value = "";
     CollectionDescriptionUpload.value = "";
-    CollectionImagesUpload.value = "";
+}
+
+function saveCollectionsToLocalStorage() {
+    localStorage.setItem("collectionsHTML", CollectionDivContainer.innerHTML);
+}
+
+function loadCollectionsFromLocalStorage() {
+    const saved = localStorage.getItem("collectionsHTML");
+    if (saved) {
+        CollectionDivContainer.innerHTML = saved;
+    }
+}
+
+function creationCollectionLink() {
+    let collections = JSON.parse(localStorage.getItem("collectionsData")) || [];
+
+    let collection = {
+        id: CollectionNameUpload.value.toLowerCase().replace(/ /g, "_"),
+        title: CollectionNameUpload.value,
+        description: CollectionDescriptionUpload.value,
+        cards: []
+    };
+
+    let existe = collections.some(col => col.id === collection.id);
+    if (existe) {
+        alert("Cette collection est existe");
+        return;
+    }
+
+    collections.push(collection);
+    localStorage.setItem("collectionsData", JSON.stringify(collections));
+
+    creerCollectionDiv();
+
+    saveCollectionsToLocalStorage();
 
     CreateCollectionForm.classList.add("hidden");
     pageContent.classList.remove("blur-2xl");
+}
+
+window.addEventListener("load", loadCollectionsFromLocalStorage);
+
+saveCollectionButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    collectionNumber++;
+    // const numberOfCollectionHome = document.getElementById("numberOfCollectionHome");
+    // numberOfCollectionHome.innerText = collectionNumber++;
+    console.log(collectionNumber);
+
+    creationCollectionLink();
 });
+
 
 
 
@@ -91,3 +123,7 @@ CollectionNameUpload.addEventListener("input" , () => {
     const collectionNumberWords = document.getElementById("collection_number--words");
     collectionNumberWords.textContent = CollectionNameUpload.value.length + "/32";
 });
+
+// window.addEventListener("load" , () => {
+//     localStorage.clear();
+// });
